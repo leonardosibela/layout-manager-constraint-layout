105307e907c32a61f41864d9558aa64adf359d64
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-26 12:14:05 -0300

    Adding group

--- app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.constraintlayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var styleGroup: Group
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.clickmeButton).setOnClickListener { onClickMeClicked() }
+        styleGroup = findViewById(R.id.styleGroup)
+    }
+
+    private fun onClickMeClicked() {
+        if (styleGroup.visibility == View.INVISIBLE) {
+            styleGroup.visibility = View.VISIBLE
+        } else {
+            styleGroup.visibility = View.INVISIBLE
+        }
     }
 }
